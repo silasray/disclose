70321d05927ac50e4af409a1351368b61d2f3dd9
Adrian Cruz <drincruz@gmail.com>
2014-08-21 23:30:33 -0400

    v0.2.6 - This fixes #1 -- There was one raise using python 2-only syntax. It's been updated to be compatibile with both python 2 and 3.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='disclose',
-      version='0.2.5',
+      version='0.2.6',
       author='Silas Ray',
       author_email='silas.ray@nytimes.com',
       url='https://github.com/silasray/disclose',
